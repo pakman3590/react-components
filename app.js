f1fa2250827e3a5ae31df010f6d5a1b13c69c9fd
Matthew Pak <mattpak@gmail.com>
2022-03-07 11:44:24 -0800

    Completed React-components

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    }
+  }
+
+  onItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    let style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
+    return (
+      <li style={style} onClick={this.onItemClick.bind(this)}>{this.props.list}</li>
+    )
+  }
+}
+
+let GroceryList = (props) => (
+    <ul>
+      {props.list.map(list =>
+        <GroceryListItem list={list} />
+      )}
+    </ul>
+)
+
+
+let App = () => (
+  <div>
+    <h2>My Groceries</h2>
+    <GroceryList list={['Potatoes', 'Carrots']} />
+  </div>
+)
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
